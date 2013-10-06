4a70728badd294d2c45d3a13eb01f5cf06987b75
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2013-10-06 12:34:56 +0200

    Extract DSL responsibilities into a ProcessGroup class

--- lib/robinhood/dsl.rb
+++ lib/robinhood/dsl.rb
@@ -1,54 +1,27 @@
-require "robinhood/process"
+require "robinhood/process_group"
 require "celluloid"
 require 'logger'
 
 module Robinhood
   class DSL
     def initialize
-      @processes = []
+      @group = ProcessGroup.new
     end
 
     def process(name, options = {}, &block)
-      @processes << [name, options, block]
+      @group.add_process(name, options, block)
     end
 
     def redis
-      if block_given?
-        @redis = yield
-      else
-        @redis ||= Redis.new
-      end
-    end
-
-    def supervision_group
-      @supervision_group ||= Class.new(Celluloid::SupervisionGroup)
+      @group.redis = yield
     end
 
     def start(options = {})
-      setup_supervision_group
-      Redis::Classy.db = redis
-
-      Robinhood.log :info, "Starting Robin Hood: Robbing from the rich and giving to the poor.."
-
-      @supervision_group_actor = if options[:background]
-                                   supervision_group.run!
-                                 else
-                                   supervision_group.run
-                                 end
+      @group.start(options)
     end
 
     def stop
-      @supervision_group_actor.finalize if @supervision_group_actor
-    end
-
-    def setup_supervision_group
-      @processes.each do |process|
-        name, options, block = process
-
-        supervision_group.supervise Process,
-          as: "robinhood_#{name}",
-          args: [name, options.merge(autostart: true), block]
-      end
+      @group.stop
     end
   end
 end
